b6d6c0066eb30a9f43a33c353c7da51c49fa14fb
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-08 16:41:02 +0600

    sign in & sign out & display content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, signOut, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from './firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSingIn =() =>{
+
     signInWithPopup(auth, provider)
     .then(result =>{
       const user = result.user;
+      setUser(user);
       console.log(user);
     })
     .catch(error =>{
@@ -18,9 +22,27 @@ function App() {
     })
   }
 
+  const handleSignOut =() =>{
+    signOut(auth)
+    .then(() =>{
+      setUser({});
+    })
+    .catch(error =>{
+      setUser({});
+    })
+  }
   return (
     <div className="App">
+      
+      {
+      user.email ? <button onClick={handleSignOut}>Sign Out</button>
+      :
       <button onClick={handleGoogleSingIn}>Google Sign In</button>
+      }
+
+      <h2>Name: {user.displayName}</h2>
+      <p>I know your email: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
